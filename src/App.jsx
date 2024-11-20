8030a34761eeffc8f049de3881654dee9a350254
afeifz <mohamedafifsmaili@gmail.com>
2024-11-20 00:44:41 -0300

    secoes concluidas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Cabecalho from "./Cabecalho";
 import ListaEnergias from "./ListaEnergias";
 import TiposEnergia from "./TiposEnergia";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<ListaEnergias />} />
         <Route path="/tipos-energia" element={<TiposEnergia />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/Footer/index.jsx
@@ -0,0 +1,7 @@
+import Contato from "../Contato";
+
+export default function Footer() {
+    return(
+        <Contato></Contato>
+    )
+}
